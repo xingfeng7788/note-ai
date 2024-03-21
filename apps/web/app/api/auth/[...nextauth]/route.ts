f2a2026d4a432cf4dcdd1a79ef48aa6e6f596467
看我也没用 <dingtianlu@qq.com>
2024-03-21 13:24:33 +0800

    Update route.ts

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -28,7 +28,7 @@ export const authOptions: NextAuthOptions = {
       // from: process.env.EMAIL_FROM,
       server: {
       host: process.env.EMAIL_SERVER_HOST,
-      port: process.env.EMAIL_SERVER_PORT,
+      port: Number(process.env.EMAIL_SERVER_PORT),
       auth: {
         user: process.env.EMAIL_SERVER_USER,
         pass: process.env.EMAIL_SERVER_PASSWORD
